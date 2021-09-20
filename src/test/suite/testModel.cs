9b36775ed8fa4d25d9bdc6a963a6ae91708ba393
Travis Williamson <travis.m.williamson@gmail.com>
2021-09-19 20:18:21 -0500

    Add test .cs file

---
+++ src/test/suite/testModel.cs
@@ -0,0 +1,17 @@
+public class TestModel
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public double Num { get; set; }
+    public bool? flag { get; set; }
+    public ChildClass ChildClass { get; set; }
+    private void HelperMethod() { }
+}
+
+public class ChildClass
+{
+    public Guid ID { get; set; }
+    public Guid TestModelID { get; set; }
+    public TestModel TestModel { get; set; }
+    public float ChildNum { get; set; }
+}
